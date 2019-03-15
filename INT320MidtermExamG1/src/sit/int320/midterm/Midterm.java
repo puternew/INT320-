8137bd59a41e3f49916618e00c3f4b99dbc85c9a
NEW <panupong.int@mail.kmutt.ac.th>
2019-03-15 14:14:19 +0700

    [UPDATE] mid test 1.

--- INT320MidtermExamG1/src/sit/int320/midterm/Midterm.java
+++ INT320MidtermExamG1/src/sit/int320/midterm/Midterm.java
@@ -5,7 +5,12 @@
  */
 package sit.int320.midterm;
 
+import java.lang.reflect.Array;
+import static java.lang.reflect.Array.set;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Midterm {
 
@@ -24,7 +29,22 @@ public class Midterm {
     }
 
     public static Object[] arrayIntersection(Object[] obj1, Object[] obj2, Comparator c) {
-        return null;
+        Set setA,setB;
+        if (c == null) {
+            setA = new TreeSet();
+            setB = new TreeSet();
+        } else {
+            setA = new TreeSet(c);
+            setB = new TreeSet(c);
+        }
+        setA.addAll(Arrays.asList(obj1));
+        setB.addAll(Arrays.asList(obj2));
+        setA.retainAll(setB);
+        return setA.toArray();
     }
-
+    public static void main(String[] args) {
+        String[] a = {"A","b","c","D"};
+        String[] b = {"a","b","C","D"};
+        System.out.println(Arrays.toString(arrayIntersection(a, b)));
+    }
 }
